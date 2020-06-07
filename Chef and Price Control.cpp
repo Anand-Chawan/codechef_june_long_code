f8c45d9372d1372f1eafafea5e85e43c5a4a3c0f
Anand-Chawan <51610419+Anand-Chawan@users.noreply.github.com>
2020-06-07 22:51:28 +0530

    code: PRICECON

--- Chef and Price Control.cpp
+++ Chef and Price Control.cpp
@@ -7,7 +7,7 @@ Chef's revenue is the sum of prices of all the items he sells. Find the amount o
 Input
 The first line of the input contains a single integer T denoting the number of test cases. The description of T test cases follows.
 The first line of each test case contains two space-separated integers N and K.
-The second line contains N space-separated integers P1,P2,…,PN.
+The second line contains N space-separated integers P1,P2,â€¦,PN.
 Output
 For each test case, print a single line containing one integer ? the amount of lost revenue.
 
@@ -35,8 +35,8 @@ Explanation
 Test Case 1: The initial revenue is 10+2+3+4+5=24. Because of the price ceiling, P1 decreases from 10 to 4 and P5 decreases from 5 to 4. The revenue afterwards is 4+2+3+4+4=17 and the lost revenue is 24-17=7.
 
 Test Case 2: The initial revenue is 1+2+3+4+5+6+7=28. For each valid i, Pi=15, so there are no changes, the revenue after introduction of the price ceiling is the same and there is zero lost revenue.
-
-Test Case 3: The initial revenue is 10+9+8+7+6=40. Since Pi>5 for each valid i, the prices of all items decrease to 5. The revenue afterwards is 5·5=25 and the lost revenue is 40-25=15.*/
+ 
+Test Case 3: The initial revenue is 10+9+8+7+6=40. Since Pi>5 for each valid i, the prices of all items decrease to 5. The revenue afterwards is 5Â·5=25 and the lost revenue is 40-25=15.*/
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
